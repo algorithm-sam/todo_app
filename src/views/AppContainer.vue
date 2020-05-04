f54d1cbb9d64224748d4a3f8d5d5052647682214
unknown <oluwatobisamuel2020@gmail.com>
2020-05-04 02:00:28 +0100

    Fixed routing for the static pages and also created the sketches for the layout

---
+++ src/views/AppContainer.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="bg-gray-100 min-w-screen min-h-screen">
+    <AppNavigation />
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import AppNavigation from "@/components/AppNavigation.vue";
+export default {
+  name: "home",
+  components: {
+    AppNavigation
+  }
+};
+</script>
+
+<style scoped>
+.hero {
+  height: 600px;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,54 @@
 <template>
-  <div class="lg:w-2/3 lg:mx-auto lg:container">
-    <h1>Hi there</h1>
+  <div>
+    <div class="hero">
+      <div class="container mx-auto">
+        <h1>This is some homepage content</h1>
+      </div>
+    </div>
+
+    <div class="description py-16">
+      <div class="container mx-auto">
+        <h1>This is a description on the left with a small underline</h1>
+        <div class="flex">
+          <div class="w-2/3">
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad error,
+              nulla similique maiores quod nemo libero ab tempora aspernatur
+              nihil earum autem rerum fugit nam minus cumque inventore in qui?
+            </p>
+          </div>
+
+          <div class="w-1/3">
+            <div>
+              <p>Image Here</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="cta">
+      <div class="container mx-auto">
+        <h1>
+          wavy background image
+        </h1>
+
+        <p>
+          Take it for a ride. Just a click away.
+        </p>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-
-export default {
-  name: "home"
-};
+export default {};
 </script>
+
+<style scoped>
+.hero {
+  height: 600px;
+  min-height: 600px;
+  @apply .bg-purple-500;
+}
+</style>
